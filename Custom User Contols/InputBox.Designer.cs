1c3dd6a4dfc4c5395fefa1ad03e76f236a51f750
Sujata <bhaleraosujata31@gmail.com>
2024-03-09 13:48:11 +0530

    master-custom control added

--- Custom User Contols/InputBox.Designer.cs
+++ Custom User Contols/InputBox.Designer.cs
@@ -29,25 +29,64 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblAlert = new System.Windows.Forms.Label();
-            this.btnOK = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.txtkey = new System.Windows.Forms.TextBox();
             this.lblerr = new System.Windows.Forms.Label();
+            this.txtkey = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblAlert = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
             this.panel1.Controls.Add(this.lblerr);
             this.panel1.Controls.Add(this.txtkey);
             this.panel1.Controls.Add(this.btnCancel);
             this.panel1.Controls.Add(this.btnOK);
             this.panel1.Controls.Add(this.lblAlert);
-            this.panel1.Location = new System.Drawing.Point(142, 101);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(381, 176);
+            this.panel1.Size = new System.Drawing.Size(478, 193);
             this.panel1.TabIndex = 0;
+            // 
+            // lblerr
+            // 
+            this.lblerr.AutoSize = true;
+            this.lblerr.ForeColor = System.Drawing.Color.Red;
+            this.lblerr.Location = new System.Drawing.Point(24, 81);
+            this.lblerr.Name = "lblerr";
+            this.lblerr.Size = new System.Drawing.Size(0, 16);
+            this.lblerr.TabIndex = 4;
+            // 
+            // txtkey
+            // 
+            this.txtkey.Location = new System.Drawing.Point(27, 56);
+            this.txtkey.Name = "txtkey";
+            this.txtkey.Size = new System.Drawing.Size(277, 22);
+            this.txtkey.TabIndex = 3;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(161, 108);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(48, 108);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // lblAlert
             // 
@@ -59,47 +98,11 @@
             this.lblAlert.TabIndex = 0;
             this.lblAlert.Text = "label1";
             // 
-            // btnOK
-            // 
-            this.btnOK.Location = new System.Drawing.Point(18, 108);
-            this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
-            this.btnOK.TabIndex = 1;
-            this.btnOK.Text = "OK";
-            this.btnOK.UseVisualStyleBackColor = true;
-            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.Location = new System.Drawing.Point(117, 108);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
-            this.btnCancel.TabIndex = 2;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
-            // 
-            // txtkey
-            // 
-            this.txtkey.Location = new System.Drawing.Point(27, 56);
-            this.txtkey.Name = "txtkey";
-            this.txtkey.Size = new System.Drawing.Size(242, 22);
-            this.txtkey.TabIndex = 3;
-            // 
-            // lblerr
-            // 
-            this.lblerr.AutoSize = true;
-            this.lblerr.ForeColor = System.Drawing.Color.Red;
-            this.lblerr.Location = new System.Drawing.Point(24, 81);
-            this.lblerr.Name = "lblerr";
-            this.lblerr.Size = new System.Drawing.Size(0, 16);
-            this.lblerr.TabIndex = 4;
-            // 
             // InputBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(478, 193);
             this.Controls.Add(this.panel1);
             this.Name = "InputBox";
             this.Text = "InputBox";

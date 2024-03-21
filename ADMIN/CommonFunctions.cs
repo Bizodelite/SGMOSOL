3a94e41fce132150bb4576243df4722756e71f60
odelites <134475710+odelites@users.noreply.github.com>
2024-03-21 12:45:45 +0530

    final master commit

--- ADMIN/CommonFunctions.cs
+++ ADMIN/CommonFunctions.cs
@@ -152,7 +152,7 @@ namespace SGMOSOL.ADMIN
                     stringBuilder.Append(", isnull(Token_Detail_code, '') AS Token_Detail_Name FROM");
                     stringBuilder.Append("  com_token_det_t");
                     stringBuilder.Append("  left join PAYMENT_TYPE_MST on PAYMENT_TYPE=Token_Detail_Id ");
-                    stringBuilder.Append(" where Token_Mst_Id = 6 and MODULE_ID=1 and STATUS=1 ");
+                    stringBuilder.Append(" where Token_Mst_Id = 6 and MODULE_ID=9 and STATUS=1 ");
                     SqlDataAdapter adapter = new SqlDataAdapter(stringBuilder.ToString(), connection);
                     connection.Close();
                     adapter.Fill(dt);

84ad4188c74aedcd544de993f95d5bc49161e6ba
Sujata <bhaleraosujata31@gmail.com>
2024-03-23 12:13:53 +0530

    hide reset button in master branch

--- SCREENS/frmLogin.Designer.cs
+++ SCREENS/frmLogin.Designer.cs
@@ -58,10 +58,10 @@
             this.pnlLogin.Controls.Add(this.label2);
             this.pnlLogin.Controls.Add(this.label1);
             this.pnlLogin.Controls.Add(this.txtUser);
-            this.pnlLogin.Location = new System.Drawing.Point(287, 141);
-            this.pnlLogin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 2);
+            this.pnlLogin.Location = new System.Drawing.Point(383, 174);
+            this.pnlLogin.Margin = new System.Windows.Forms.Padding(3, 0, 3, 2);
             this.pnlLogin.Name = "pnlLogin";
-            this.pnlLogin.Size = new System.Drawing.Size(459, 279);
+            this.pnlLogin.Size = new System.Drawing.Size(612, 343);
             this.pnlLogin.TabIndex = 0;
             // 
             // pictureBox1
@@ -71,9 +71,9 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(459, 73);
+            this.pictureBox1.Size = new System.Drawing.Size(612, 90);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 35;
             this.pictureBox1.TabStop = false;
@@ -84,10 +84,10 @@
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClose.ForeColor = System.Drawing.Color.GhostWhite;
-            this.btnClose.Location = new System.Drawing.Point(308, 210);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClose.Location = new System.Drawing.Point(411, 258);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(83, 37);
+            this.btnClose.Size = new System.Drawing.Size(111, 46);
             this.btnClose.TabIndex = 6;
             this.btnClose.Text = "CLOSE";
             this.btnClose.UseVisualStyleBackColor = false;
@@ -96,13 +96,13 @@
             // lnkRestPassword
             // 
             this.lnkRestPassword.AutoSize = true;
-            this.lnkRestPassword.Location = new System.Drawing.Point(305, 258);
-            this.lnkRestPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lnkRestPassword.Location = new System.Drawing.Point(407, 318);
             this.lnkRestPassword.Name = "lnkRestPassword";
-            this.lnkRestPassword.Size = new System.Drawing.Size(84, 13);
+            this.lnkRestPassword.Size = new System.Drawing.Size(106, 16);
             this.lnkRestPassword.TabIndex = 5;
             this.lnkRestPassword.TabStop = true;
             this.lnkRestPassword.Text = "Reset Password";
+            this.lnkRestPassword.Visible = false;
             this.lnkRestPassword.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkRestPassword_LinkClicked);
             // 
             // lblmessage
@@ -110,20 +110,19 @@
             this.lblmessage.AutoSize = true;
             this.lblmessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblmessage.ForeColor = System.Drawing.Color.Red;
-            this.lblmessage.Location = new System.Drawing.Point(20, 176);
-            this.lblmessage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblmessage.Location = new System.Drawing.Point(27, 217);
             this.lblmessage.Name = "lblmessage";
-            this.lblmessage.Size = new System.Drawing.Size(111, 17);
+            this.lblmessage.Size = new System.Drawing.Size(130, 20);
             this.lblmessage.TabIndex = 4;
             this.lblmessage.Text = "User message";
             // 
             // txtpwd
             // 
             this.txtpwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtpwd.Location = new System.Drawing.Point(144, 146);
-            this.txtpwd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtpwd.Location = new System.Drawing.Point(192, 180);
+            this.txtpwd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtpwd.Name = "txtpwd";
-            this.txtpwd.Size = new System.Drawing.Size(228, 22);
+            this.txtpwd.Size = new System.Drawing.Size(303, 26);
             this.txtpwd.TabIndex = 3;
             this.txtpwd.UseSystemPasswordChar = true;
             // 
@@ -133,10 +132,10 @@
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.ForeColor = System.Drawing.Color.GhostWhite;
-            this.btnLogin.Location = new System.Drawing.Point(220, 210);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLogin.Location = new System.Drawing.Point(293, 258);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(83, 37);
+            this.btnLogin.Size = new System.Drawing.Size(111, 46);
             this.btnLogin.TabIndex = 1;
             this.btnLogin.Text = "LOGIN";
             this.btnLogin.UseVisualStyleBackColor = false;
@@ -147,10 +146,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.DarkBlue;
-            this.label2.Location = new System.Drawing.Point(39, 149);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(52, 183);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.Size = new System.Drawing.Size(115, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "PASSWORD";
             // 
@@ -159,20 +157,19 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DarkBlue;
-            this.label1.Location = new System.Drawing.Point(39, 113);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(52, 139);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.Size = new System.Drawing.Size(111, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "USERNAME";
             // 
             // txtUser
             // 
             this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUser.Location = new System.Drawing.Point(144, 110);
-            this.txtUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUser.Location = new System.Drawing.Point(192, 135);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(228, 22);
+            this.txtUser.Size = new System.Drawing.Size(303, 26);
             this.txtUser.TabIndex = 1;
             // 
             // pctControls
@@ -182,21 +179,21 @@
             this.pctControls.Dock = System.Windows.Forms.DockStyle.Top;
             this.pctControls.Image = ((System.Drawing.Image)(resources.GetObject("pctControls.Image")));
             this.pctControls.Location = new System.Drawing.Point(0, 0);
-            this.pctControls.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pctControls.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pctControls.Name = "pctControls";
-            this.pctControls.Size = new System.Drawing.Size(1028, 136);
+            this.pctControls.Size = new System.Drawing.Size(1371, 167);
             this.pctControls.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pctControls.TabIndex = 34;
             this.pctControls.TabStop = false;
             // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1028, 609);
+            this.ClientSize = new System.Drawing.Size(1371, 750);
             this.Controls.Add(this.pnlLogin);
             this.Controls.Add(this.pctControls);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmLogin";
             this.Text = "frmLogin";
             this.Load += new System.EventHandler(this.frmLogin_Load);
